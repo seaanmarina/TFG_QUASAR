633b63d810925e3e343b491884c7b866207078f9
seaanmarina <104677546+seaanmarina@users.noreply.github.com>
2022-11-29 15:17:37 +0100

    Empezando multijugador sincrono

--- Assets/Scenes/Launcher.cs
+++ Assets/Scenes/Launcher.cs
@@ -7,7 +7,12 @@ public class Launcher : MonoBehaviourPunCallbacks
 {
 
     public PhotonView playerPrefab;
+    public GameObject contgame;
+     Contador_Jug contador; 
 
+
+    public Transform spawn1;
+    public Transform spawn2;
 
     public float minX;
     public float maxX;
@@ -16,6 +21,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     public float minZ;
     public float maxZ;
 
+    private int spawnIndex=1;
 
     private Vector3 randomPosition;
     // Start is called before the first frame update
@@ -23,7 +29,10 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
 
          randomPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+
+        contador = contgame.GetComponent<Contador_Jug>();
         
+
         //try to connect
         PhotonNetwork.ConnectUsingSettings();
     }
@@ -38,12 +47,32 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("Joined a room succesfully");
-        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+        Debug.Log(contador.contador);
+
+        if (contador.contador == 1)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, spawn1.position, Quaternion.identity);
+            contador.contador--;
+        }
+        else
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, spawn2.position, Quaternion.identity);
+            contador.contador--;
+        }
+
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+
+
+
+
         
     }
 }
