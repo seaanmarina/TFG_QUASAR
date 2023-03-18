fb098f73ef959c5ba93f83bbce77ab97ccfdf87a
seaanmarina <104677546+seaanmarina@users.noreply.github.com>
2023-03-18 14:56:23 +0100

    No se ven los jugadores
    Entre los jugadores ya no se ven

--- Assets/Scripts/Camera_Follow.cs
+++ Assets/Scripts/Camera_Follow.cs
@@ -1,5 +1,9 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class Camera_Follow : MonoBehaviour
 {
@@ -8,13 +12,49 @@ public class Camera_Follow : MonoBehaviour
     public float z;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-   
 
+    public GameObject capsula;
+    public GameObject ojo;
+
+    private GameObject playerCharacter;
     void Start()
     {
 
+        playerCharacter = GameObject.FindGameObjectWithTag("Player");
+        //playerCharacter.layer = LayerMask.NameToLayer("Jugador");
+
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Player"))
+        {
+
+            
+            if (go.GetComponent<PhotonView>().Owner.ActorNumber != PhotonNetwork.LocalPlayer.ActorNumber)
+            {
+
+
+                GameObject childCapsule = go.transform.Find("Capsule").gameObject;
+                GameObject childTransform = childCapsule.transform.Find("Sphere").gameObject;
+                go.layer = LayerMask.NameToLayer("Jugador");
+                childCapsule.layer = LayerMask.NameToLayer("Jugador");
+                childTransform.layer = LayerMask.NameToLayer("Jugador");
+
+                //go.layer = LayerMask.NameToLayer("Jugador");
+                //    PhotonView view = go.GetComponent<PhotonView>();
+                //    if (view != null)
+                //    {
+                //        view.ObservedComponents = new List<Component>();
+                //        view.Synchronization = ViewSynchronization.Off;
+            }
+            //}
+        }
+
+
+
         z = transform.position.z;
     }
+
+
+    
+ 
 
     void Update()
     {
