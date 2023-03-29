1b6043502c841430552dff89c689e046fe813b8a
seaanmarina <104677546+seaanmarina@users.noreply.github.com>
2023-03-29 22:09:54 +0200

    Caminar controlado

--- Assets/ScriptsASIN/CaminarA.cs
+++ Assets/ScriptsASIN/CaminarA.cs
@@ -19,6 +19,10 @@ public class CaminarA : MonoBehaviour
     public Camera cam;
 
 
+
+    public int iniciocaminar;
+
+
    // public GameObject ObjetoControllador;
     InputHandlerA _inputHandler;
     private CharacterController controller;
@@ -26,13 +30,17 @@ public class CaminarA : MonoBehaviour
     private float verticalVelocity;
     public float gravity = 25;
     public float jumpforce = 8;
-
+    public float valorprueba;
 
     public float speed = 8;
     private Vector2 lastMove;
     // Start is called before the first frame update
     void Start()
     {
+
+        iniciocaminar = 0;
+
+
         controller = GetComponent<CharacterController>();
         _inputHandler = GetComponent<InputHandlerA>();
         //cam = Camera.main;
@@ -53,13 +61,57 @@ public class CaminarA : MonoBehaviour
 
            // Debug.Log(speed);
             moveVector = Vector2.zero;
-            moveVector.x = _inputHandler.Horizontal * speed;
+           // moveVector.x = _inputHandler.Horizontal * speed;
+
+            if (_inputHandler.Horizontal > 0)
+            {
+                
+                valorprueba += ((_inputHandler.Horizontal*speed) / 0.2f) * Time.deltaTime;
+                valorprueba = Mathf.Min(valorprueba, speed);
+                iniciocaminar = 1;
+                moveVector.x = valorprueba;
+            }
+            else if(_inputHandler.Horizontal < 0)
+            {
+                valorprueba += ((_inputHandler.Horizontal * speed) / 0.2f) * Time.deltaTime;
+                valorprueba = Mathf.Max(valorprueba, -speed);
+                iniciocaminar = 2;
+                moveVector.x = valorprueba;
+                //TO DO: PARA CADA UNO TOMARLO DIERENTE PORQUE UNO VA HACIA LA IZQUIERDA Y OTRO VA HACIA LA DERECHA. 
+            }
+            else if(_inputHandler.Horizontal == 0 && iniciocaminar==1)
+            {
+                valorprueba -= (speed / 0.2f) * Time.deltaTime;
+                valorprueba = Mathf.Max(valorprueba, 0);
+                moveVector.x = valorprueba;
+                //iniciocaminar = true;
+            }
+            else if (_inputHandler.Horizontal == 0 && iniciocaminar == 2)
+            {
+                valorprueba += (speed / 0.2f) * Time.deltaTime;
+                valorprueba = Mathf.Min(valorprueba, 0);
+                moveVector.x = valorprueba;
+                //iniciocaminar = true;
+            }
 
 
+            //moveVector.x = Mathf.Lerp(moveVector.x, _inputHandler.Horizontal * speed, 6 / 2);
             Vector3 dir = new Vector3(moveVector.x, 0, 0).normalized;
 
 
+            /*if(_inputHandler.Horizontal != 0 && iniciocaminar)
+            {
+                iniciocaminar = false;
+                StartCoroutine(AcelerarCaminar());
+                
 
+
+
+            }*/
+            float changeRatePerSecond = 1 / 3 * Time.deltaTime;
+
+
+            valorprueba  = Mathf.MoveTowards(valorprueba, speed, changeRatePerSecond);
 
 
 
@@ -126,7 +178,35 @@ public class CaminarA : MonoBehaviour
 
     }
 
-   
+
+    private IEnumerator AcelerarCaminar()
+    {
+        float currentTime = 0;
+
+        valorprueba = 0;
+
+
+
+        while (currentTime <= 5)
+        {
+            ////https://answers.unity.com/questions/1038571/colorlerp-for-spriterender-is-not-smooth.html
+            currentTime += Time.deltaTime;
+            float lerp_Percentage = currentTime / 5;
+
+
+            //  Debug.Log("COOORRRUUUTIIINA");
+            // input_player
+            Debug.Log(valorprueba + "CAMINAR");
+            valorprueba = Mathf.Lerp(valorprueba, _inputHandler.Horizontal * speed, lerp_Percentage);
+            yield return null;
+
+           
+        }
+
+
+
+    }
+
 
 
 
