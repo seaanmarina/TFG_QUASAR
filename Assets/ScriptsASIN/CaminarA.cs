a776b3a98eefa683103f7cd9200c05ee11cc8daa
seaanmarina <104677546+seaanmarina@users.noreply.github.com>
2023-04-03 15:15:50 +0200

    Dash controlado

--- Assets/ScriptsASIN/CaminarA.cs
+++ Assets/ScriptsASIN/CaminarA.cs
@@ -18,6 +18,11 @@ public class CaminarA : MonoBehaviour
 
     public Camera cam;
 
+    private TrailRenderer cola;
+
+    public GameObject particulas;
+    ParticleSystem sistema;
+
 
 
     public int iniciocaminar;
@@ -32,12 +37,25 @@ public class CaminarA : MonoBehaviour
     public float jumpforce = 8;
     public float valorprueba;
 
+    float principiodash = 0;
+
+    public float valordash;
+
     public float speed = 8;
     private Vector2 lastMove;
     // Start is called before the first frame update
     void Start()
     {
 
+        sistema = particulas.GetComponent<ParticleSystem>();
+
+        sistema.startSize = 0;
+        valordash= 0;
+
+
+        cola = GetComponent<TrailRenderer>();
+
+        cola.enabled = false;
         iniciocaminar = 0;
 
 
@@ -63,11 +81,11 @@ public class CaminarA : MonoBehaviour
             moveVector = Vector2.zero;
            // moveVector.x = _inputHandler.Horizontal * speed;
 
-            if (_inputHandler.Horizontal > 0)
+            if (_inputHandler.Horizontal > 0) //SE COMPRUEBA SI SE VA PARA LA IZQUIERDA O VA PARA LA DERECHA
             {
                 
                 valorprueba += ((_inputHandler.Horizontal*speed) / 0.2f) * Time.deltaTime;
-                valorprueba = Mathf.Min(valorprueba, speed);
+                valorprueba = Mathf.Min(valorprueba, speed); //COGE EL VALOR MINIMO
                 iniciocaminar = 1;
                 moveVector.x = valorprueba;
             }
@@ -79,7 +97,7 @@ public class CaminarA : MonoBehaviour
                 moveVector.x = valorprueba;
                 //TO DO: PARA CADA UNO TOMARLO DIERENTE PORQUE UNO VA HACIA LA IZQUIERDA Y OTRO VA HACIA LA DERECHA. 
             }
-            else if(_inputHandler.Horizontal == 0 && iniciocaminar==1)
+            else if(_inputHandler.Horizontal == 0 && iniciocaminar==1) //DEPENDIENDO DE SI VA PARA LA IZQUIERDA O DERECHA HARA UNA COSA U OTRA YA QUE HABRÁN NUMEROS EN NEGATIVO
             {
                 valorprueba -= (speed / 0.2f) * Time.deltaTime;
                 valorprueba = Mathf.Max(valorprueba, 0);
@@ -118,14 +136,31 @@ public class CaminarA : MonoBehaviour
             if (moveVector.magnitude > 0)
             {
                 float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+
                 transform.LookAt(transform.position + new Vector3(0, 0, moveVector.x));//mira a un punto
-                moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+               // moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
             }
 
             if (_inputHandler._dash)
             {
-                StartCoroutine(Dash());
+
+                
+
+
+                    sistema.startSize = 1;
+                    //valordash += (dashSpeed / 5f) * Time.deltaTime;
+                    //valordash = Mathf.Min(valordash, dashSpeed);
+                //COGE EL VALOR MINIMO
+                //Debug.Log("InicioDash");
+                StartCoroutine(InicioDash());
+                //Debug.Log("FinalDash");
+                //valordash -= (dashSpeed / 5f) * Time.deltaTime;
+                //valordash = Mathf.Max(valorprueba, 0);
+                //moveVector.x = valordash;
+                //sistema.startSize = 0;
                 _inputHandler._dash = false;
+                
             }
             else
             {
@@ -164,48 +199,113 @@ public class CaminarA : MonoBehaviour
         }
     }
 
+    private IEnumerator ConjuntoDash()
+    {
+        yield return StartCoroutine(InicioDash());
+        yield return  StartCoroutine(Dash());
+        yield return  StartCoroutine(FinalDash());
+        yield return null;
+
+    }
+
+
+    private IEnumerator InicioDash()
+    {
+        float currentTime = 0;
+        
+        //while (currentTime < t)
+        //{
+        float time = 0.20f;
+        while (currentTime <= time)
+        {
+            currentTime += Time.deltaTime;
+            float lerp_Percentage = currentTime / time;
+            Vector3 dashDirection = Vector3.Cross(transform.forward, Vector3.up).normalized;
+
+            valordash = Mathf.Lerp(principiodash, dashSpeed, lerp_Percentage);
+            //valordash = Mathf.Lerp(valordash, dashSpeed, lerp_Percentage); ASI ESTABA ANTES, COMO EL VALOR DE VALOR DASH NO PARABA DE ACTUALIZARSE, NO SE ESTABA HACIENDO CORRECTAMENTE
+            
+            //moveVector.x = valorprueba;
+            controller.Move(-dashDirection * valordash * Time.deltaTime);
+
+            Debug.Log("dentro del While Dash");
+            
+            
+            //valordash = Mathf.Lerp(valordash, dashSpeed, lerp_Percentage);
+            //controller.Move(-dashDirection * valordash * Time.deltaTime);
+            yield return null;
+        }
+        principiodash = valordash;
+        //moveVector.x = valordash;
+        yield return StartCoroutine(Dash());
+
+    }
+
+
+    private IEnumerator FinalDash()
+    {
+        float currentTime = 0;
+
+        //while (currentTime < t)
+        //{
+        float time = 0.1f;
+        while (currentTime <= time)
+        {
+            currentTime += Time.deltaTime;
+            float lerp_Percentage = currentTime / time;
+            Vector3 dashDirection = Vector3.Cross(transform.forward, Vector3.up).normalized;
+
+            valordash = Mathf.Lerp(principiodash, 0, lerp_Percentage);
+            //valordash = Mathf.Lerp(valordash, dashSpeed, lerp_Percentage); ASI ESTABA ANTES, COMO EL VALOR DE VALOR DASH NO PARABA DE ACTUALIZARSE, NO SE ESTABA HACIENDO CORRECTAMENTE
+
+            //moveVector.x = valorprueba;
+            controller.Move(-dashDirection * valordash * Time.deltaTime);
+
+            Debug.Log("Final");
+
+
+            //valordash = Mathf.Lerp(valordash, dashSpeed, lerp_Percentage);
+            //controller.Move(-dashDirection * valordash * Time.deltaTime);
+            yield return null;
+        }
+        principiodash = valordash;
+        sistema.startSize = 0;
+    }
+
+
+
+
+
     private IEnumerator Dash()
     {
         float startTime = Time.time;
 
-        while(Time.time < startTime + dashTime)
+        Debug.Log("Dash");
+
+
+        while (Time.time < startTime + dashTime)
         {
-            controller.Move(moveDir * dashSpeed * Time.deltaTime);
+            //cola.enabled = true;
+            Vector3 dashDirection = Vector3.Cross(transform.forward, Vector3.up).normalized;
+            controller.Move(-dashDirection * dashSpeed * Time.deltaTime);
+           // controller.Move(transform.forward * dashSpeed * Time.deltaTime);
 
             yield return null;
-        }
-
-
-    }
-
-
-    private IEnumerator AcelerarCaminar()
-    {
-        float currentTime = 0;
-
-        valorprueba = 0;
 
 
 
-        while (currentTime <= 5)
-        {
-            ////https://answers.unity.com/questions/1038571/colorlerp-for-spriterender-is-not-smooth.html
-            currentTime += Time.deltaTime;
-            float lerp_Percentage = currentTime / 5;
-
-
-            //  Debug.Log("COOORRRUUUTIIINA");
-            // input_player
-            Debug.Log(valorprueba + "CAMINAR");
-            valorprueba = Mathf.Lerp(valorprueba, _inputHandler.Horizontal * speed, lerp_Percentage);
-            yield return null;
-
-           
         }
 
 
 
+        yield return StartCoroutine(FinalDash());
+        //cola.enabled = false;
+        //sistema.startSize = 0;
+
     }
+
+
+    
 
 
 
